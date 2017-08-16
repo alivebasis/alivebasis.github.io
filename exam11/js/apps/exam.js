8ad9a0228ddd67f974ffd1393614aa2afcd76d73
leejaegeun <alivebasis@hivelab.co.kr>
2017-08-16 19:06:58 +0900

    exam11

--- exam11/js/apps/exam.js
+++ exam11/js/apps/exam.js
@@ -5,28 +5,32 @@
 
     var UTIL = win.exam.common.util;
 
-    win.exam.chkbox = function (args) {
+    // checkbox
+    win.exam.chkboxFunc = function (args) {
         var defParams = {
-            container : '.check_box'
+            container : '.check_box',
+            activeClass : 'chk_active'
         }
         this.opts = UTIL.def(defParams, (args || {}));
         if(!(this.obj = $(this.opts.container)).length) return;
         this.init();
     };
-    win.exam.chkbox.prototype = {
+    win.exam.chkboxFunc.prototype = {
         init : function () {
             this.setElements();
             this.initLayout();
             this.bindEvents();
         },
         setElements : function () {
-            this.input = this.obj.find('.ipt_chk');
+            this.chkbox = this.obj;
+            this.input = this.chkbox.find('.ipt_chk');
+            this.activeClass = this.opts.activeClass;
         },
         initLayout : function () {
             console.log(this.input.filter(':checked'));
             console.log(this.input.is(':checked'));
-            this.input.filter(':checked').closest('.check_box').addClass('chk_active');
-            this.input.filter(':not(:checked)').closest('.check_box').removeClass('chk_active');
+            this.input.filter(':checked').closest(this.chkbox).addClass(this.activeClass);
+            this.input.filter(':not(:checked)').closest(this.chkbox).removeClass(this.activeClass);
         },
         bindEvents : function () {
             this.input.on('change', $.proxy(this.onChangeChk, this));
@@ -34,12 +38,92 @@
         onChangeChk : function (e) {
             var target = $(e.currentTarget);
             //this.input 아닌 target으로 지정해야, 해당 체크박스에 각각 적용
-            if(target.is(':checked')){
-                target.closest('.check_box').addClass('chk_active').prop('checked', true);
-            }else{
+            console.log(target);
+            if (target.is(':checked')) {
+                target.closest(this.chkbox).addClass(this.activeClass).prop('checked', true);
+            } else {
                 //toggle prop이 removeProp?
-                target.closest('.check_box').removeClass('chk_active').prop('checked', false); 
+                target.closest(this.chkbox).removeClass(this.activeClass).prop('checked', false); 
             }
         }
     };
+
+    // radiobox
+    win.exam.rdoboxFunc = function (args) {
+        var defParams = {
+            container : '.radio_box',
+            activeClass : 'chk_active'
+        }
+        this.opts = UTIL.def(defParams, (args || {}));
+        if(!(this.obj = $(this.opts.container)).length) return;
+        this.init();
+    };
+    win.exam.rdoboxFunc.prototype = {
+        init : function () {
+            this.setElements();
+            this.initLayout();
+            this.bindEvents();
+        },
+        setElements : function () {
+            this.rdoBox = this.obj;
+            this.input = this.rdoBox.find('.ipt_rdo');
+            this.activeClass = this.opts.activeClass;
+        },
+        initLayout : function () {
+            this.input.filter(':checked').closest(this.rdoBox).addClass(this.activeClass);
+            this.input.filter(':not(:checked)').closest(this.rdoBox).removeClass(this.activeClass);
+        },
+        bindEvents : function () {
+            this.input.on('change', $.proxy(this.onChangeChk, this));
+        },
+        onChangeChk : function (e) {
+            var _this = this,
+                target = $(e.currentTarget);
+            this.input.filter(function () {
+                if (target.attr('name') === $(this).attr('name')) {
+                    $(this).closest(_this.rdoBox).toggleClass(_this.activeClass, $(this).prop('checked'));
+                }
+            });
+        }
+    };
+
+    // selectbox
+    win.exam.selboxFunc = function (args) {
+        var defParams = {
+            container : '.select_box',
+            activeClass : 'chk_active'
+        }
+        this.opts = UTIL.def(defParams, (args || {}));
+        if(!(this.obj = $(this.opts.container)).length) return;
+        this.init();
+    };
+    win.exam.selboxFunc.prototype = {
+        init : function () {
+            this.setElements();
+            this.initLayout();
+            this.bindEvents();
+        },
+        setElements : function () {
+            this.selBox = this.obj;
+            this.input = this.selBox.find('.ipt_rdo');
+            this.activeClass = this.opts.activeClass;
+        },
+        initLayout : function () {
+            this.input.filter(':checked').closest(this.selBox).addClass(this.activeClass);
+            this.input.filter(':not(:checked)').closest(this.selBox).removeClass(this.activeClass);
+        },
+        bindEvents : function () {
+            this.input.on('change', $.proxy(this.onChangeChk, this));
+        },
+        onChangeChk : function (e) {
+            var _this = this,
+                target = $(e.currentTarget);
+            this.input.filter(function () {
+                if (target.attr('name') === $(this).attr('name')) {
+                    $(this).closest(_this.selBox).toggleClass(_this.activeClass, $(this).prop('checked'));
+                }
+            });
+        }
+    };
+
 })(window, window.jQuery);
